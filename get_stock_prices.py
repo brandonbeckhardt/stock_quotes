4834a5b50f706793a5c50ce7f205b229ee6149dc
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2019-08-08 23:27:29 -0700

    Can update Most Recent Price based on Symbol in Current Holdings

--- get_stock_prices.py
+++ get_stock_prices.py
@@ -2,20 +2,40 @@ import os
 import sys
 import requests
 import pprint
+# from airtable-python-wrapper import Airtable
+from airtable import Airtable
+import unirest
 
 apiKey="60BSELJ4STUEN8NZ"
 pp = pprint.PrettyPrinter(indent=4)
 
+
+
+
 def main():
-	jsonObj={
-	"apikey":apiKey,
-	"function":"TIME_SERIES_DAILY_ADJUSTED",
-	"symbol":"MSFT"
-	}
-	print(jsonObj)
-	r = requests.get('https://www.alphavantage.co/query', jsonObj)
-	r.status_code
-	# print(r.json())
-	pp.pprint(r.json())
+
+
+	airtable = Airtable('appLWRsE1SsJOsoOP', 'Current Holdings',api_key='keyOFzjy926o6Hx04')
+	airtable.get_all()
+	holdings = airtable.get_all(formula="Symbol")
+
+	for holding in holdings:
+		
+		symbol = holding['fields']['Symbol'][0]
+		# print(jsonObj)
+		
+		r = requests.get("https://sandbox.tradier.com/v1/markets/quotes",
+			{"symbols":symbol},
+		  headers={
+		    "Accept":"application/json",
+	         "Authorization":"Bearer fr0EicWidzcRf3RAPzCmGNQJKezf"
+	 	 	}
+		)
+		quoteInfo=r.json()['quotes']['quote']
+		recentAskPrice = quoteInfo['ask']
+		recordId=holding['id']
+		print("Symbol: " + symbol + " - Ask Price: " + str(recentAskPrice))
+		fields={'Most Recent Price': recentAskPrice}
+		airtable.update(recordId, fields)
 	return
 main()
